96fafd588d654ea65b3dd2ab2e79565d5b0599d3
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-02-29 14:33:27 +0900

    basic認証練習

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   private
   def basic
     authenticate_or_request_with_http_basic do |name, password|
-      name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
+      name == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
