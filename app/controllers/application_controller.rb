36b8bf75639efbf16c507f43895944acb5f9770e
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-02-29 14:35:18 +0900

    remove basic認証の実装は保留

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic
-  protect_from_forgery with: :exception
-  private
-  def basic
-    authenticate_or_request_with_http_basic do |name, password|
-      name == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
-    end
-  end
 end
