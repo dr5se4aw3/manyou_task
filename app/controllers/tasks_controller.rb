7595e420759615f566b9e4949752bccc3c7a889b
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-03 14:21:18 +0900

    Add CRUD機能にdeadlineカラムを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -69,6 +69,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :detail)
+      params.require(:task).permit(:title, :detail, :deadline)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,5 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 30 }
   validates :detail, presence: true, length: { maximum: 255 }
+  validates :deadline, presence: true
 end
