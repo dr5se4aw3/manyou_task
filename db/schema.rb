36fc20a68362cfd81dbe1daa8d7f0db4ec77b8b6
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-03 14:00:45 +0900

    Add taskテーブルにdeadlinカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_29_025905) do
+ActiveRecord::Schema.define(version: 2020_03_03_044500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_02_29_025905) do
     t.text "detail", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "deadline", default: "1000-01-01", null: false
   end
 
 end
