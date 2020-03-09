bac8175fe578e1c43ebf90857a88f554bfdd7d99
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-09 10:50:40 +0900

    Add userテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_052520) do
+ActiveRecord::Schema.define(version: 2020_03_09_013538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_03_04_052520) do
     t.string "status", default: "未着手", null: false
     t.integer "priority", default: 0, null: false
     t.index ["title", "status"], name: "index_tasks_on_title_and_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
