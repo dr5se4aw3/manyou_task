4aab2354e1480422f2ab20dc2ad43c8a538bbd21
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-10 11:03:39 +0900

    Add 管理者用ユーザー一覧を作成、ユーザー情報とユーザーのタスク数が表示される

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,36 +1,26 @@
 class Admin::UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :check_login, only: [:show, :edit, :update, :destroy]
+  before_action :check_admin, only: [:show, :edit, :update, :destroy]
   # GET /users
   # GET /users.json
-=begin
-  #一般ユーザーではユーザー一覧機能は使用できない
-  #別途、管理者用コントローラーを作成し、そちらでは一覧機能を実装する
   def index
     @users = User.all
   end
-=end
   # GET /users/1
   # GET /users/1.json
   def show
-    if @user.id != current_user.id
-      redirect_to user_path(current_user.id), notice: '他ユーザーの情報は閲覧できません。'
-    end
   end
 
   # GET /users/new
   def new
     if logged_in?
-      redirect_to user_path(current_user.id), notice: '既にログインしています。'
+      redirect_to admin_user_path(current_user.id), notice: '既にログインしています。'
     end
     @user = User.new
   end
 
   # GET /users/1/edit
   def edit
-    if @user.id != current_user.id
-      redirect_to user_path(current_user.id), notice: '他ユーザーの情報は編集できません。'
-    end
   end
 
   # POST /users
@@ -40,7 +30,7 @@ class Admin::UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました。"
-      redirect_to user_path(@user.id)
+      redirect_to admin_user_path(@user.id)
     else
       render :new
     end
@@ -49,27 +39,19 @@ class Admin::UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        redirect_to admin_user_path(@user.id), notice: 'ユーザー情報の更新が完了しました'
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
-    if @user.id != current_user.id
-      redirect_to user_path(current_user.id), notice: '他ユーザーの情報は削除できません。'
-    end
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to new_user_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+      redirect_to admin_users_path, notice: 'ユーザーの削除が完了しました'
     end
   end
 
@@ -81,12 +63,12 @@ class Admin::UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
-    def check_login
-      if logged_in?
+    def check_admin
+      if admin_user?
       else
-        flash[:notice] = "ログインしてください"
+        flash[:notice] = "管理者権限がありません"
         redirect_to new_session_path
       end
     end
