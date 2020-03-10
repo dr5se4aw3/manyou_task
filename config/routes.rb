900f02f7bc5b4874be310fc2d1bb1799c30748ef
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-10 10:19:25 +0900

    Add Admin用ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :tasks
   #ユーザー管理のルーティング
   resources :users
+  namespace :admin do
+    resources :users
+  end
   #セッション管理のルーティング
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
