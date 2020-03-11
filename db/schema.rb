d176bdd19ecff8dfde94646c8443aea8d3143f6a
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-11 15:44:57 +0900

    Add labelテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_063449) do
+ActiveRecord::Schema.define(version: 2020_03_11_064109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2020_03_11_063449) do
     t.datetime "updated_at", null: false
     t.index ["task_id"], name: "index_label_on_tasks_on_task_id"
     t.index ["user_id"], name: "index_label_on_tasks_on_user_id"
+  end
+
+  create_table "labels", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
