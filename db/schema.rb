efcbdf7d27b2ce65d3ce88b7066e83d39061d28b
hiroki matsubara <dr5se4aw3@yahoo.com>
2020-03-11 15:39:16 +0900

    Add ラベルとタスクを多対多で紐づけるテーブル(LabelOnTask)を作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_022304) do
+ActiveRecord::Schema.define(version: 2020_03_11_063449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "label_on_tasks", force: :cascade do |t|
+    t.bigint "task_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_label_on_tasks_on_task_id"
+    t.index ["user_id"], name: "index_label_on_tasks_on_user_id"
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
@@ -37,5 +46,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_022304) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "label_on_tasks", "tasks"
+  add_foreign_key "label_on_tasks", "users"
   add_foreign_key "tasks", "users"
 end
---
+++ spec/factories/label_on_tasks.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :label_on_task do
+    
+  end
+end
